b14a9918a2a841e4befb7b4d217b1b6dbc434fe1
Casper Rækjær <casper@raekjaer.dk>
2013-09-14 11:13:26 +0200

    Add GA code for anonymous users.

--- functions.php
+++ functions.php
@@ -15,4 +15,24 @@ function twentythirteen_child_custom_header_setup() {
 }
 // add it the same way Twenty Thirteen does
 add_action( 'after_setup_theme', 'twentythirteen_child_custom_header_setup' );
+
+/**
+ * Add Google Analytics tracking code to footer.
+ * See http://www.wpbeginner.com/beginners-guide/how-to-install-google-analytics-in-wordpress/
+ */
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() {
+  if (is_user_logged_in() == FALSE) {
+    echo "<script>" .
+      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" .
+      "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," .
+      "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" .
+      "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" .
+      "ga('create', 'UA-44041803-1', 'joernsdagbog.dk');" .
+      "ga('send', 'pageview');" .
+    "</script>" .
+    "\n";
+  }
+}
+
 ?>
