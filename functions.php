55a5cede9ad0787c296ad95f8f5d402048bb7382
Casper Rækjær <casper@raekjaer.dk>
2013-09-17 12:15:47 +0200

    Add custom field for GA tracking code.

--- functions.php
+++ functions.php
@@ -19,16 +19,33 @@ add_action( 'after_setup_theme', 'twentythirteen_child_custom_header_setup' );
 /**
  * Add Google Analytics tracking code to footer.
  * See http://www.wpbeginner.com/beginners-guide/how-to-install-google-analytics-in-wordpress/
+ * and http://codegurus.eu/2012/01/add-custom-fields-under-wp-general-settings/
  */
+
+// Add GA field to general options
+add_filter('admin_init', 'ga_general_settings_register_fields');
+function ga_general_settings_register_fields()
+{
+    register_setting('general', 'ga_field', 'esc_attr');
+    add_settings_field('ga_field', '<label for="ga_field">'.__('Google Analytics tracking ID' , 'my_field' ).'</label>' , 'ga_general_settings_fields_html', 'general');
+} 
+function ga_general_settings_fields_html()
+{
+    $value = get_option( 'ga_field', '' );
+    echo '<input type="text" id="ga_field" name="ga_field" value="' . $value . '" />';
+}
+
+// Add GA tracking code to footer part of page
 add_action('wp_footer', 'add_googleanalytics');
 function add_googleanalytics() {
-  if (is_user_logged_in() == FALSE) {
+  $ga_tracking_id = get_option('ga_field', FALSE);
+  if (is_user_logged_in() == FALSE && $ga_tracking_id != FALSE) {
     echo "<script>" .
       "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" .
       "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," .
       "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" .
       "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" .
-      "ga('create', 'UA-44041803-1', 'joernsdagbog.dk');" .
+      "ga('create', '" . $ga_tracking_id . "', 'joernsdagbog.dk');" .
       "ga('send', 'pageview');" .
     "</script>" .
     "\n";
