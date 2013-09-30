9d8cfd0d73ab51f3f2e8df0b9b6e1679c6013681
Casper Rækjær <casper@raekjaer.dk>
2013-09-30 10:31:01 +0200

    Add old analytics code + set domain name from server name.

--- functions.php
+++ functions.php
@@ -40,13 +40,28 @@ add_action('wp_footer', 'add_googleanalytics');
 function add_googleanalytics() {
   $ga_tracking_id = get_option('ga_field', FALSE);
   if (is_user_logged_in() == FALSE && $ga_tracking_id != FALSE) {
+    // Get current domain name
+    $domain_name =  preg_replace('/^www\./','',$_SERVER['SERVER_NAME']);
+    // New Universal analytics.js code
     echo "<script>" .
       "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" .
       "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," .
       "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" .
       "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" .
-      "ga('create', '" . $ga_tracking_id . "', 'joernsdagbog.dk');" .
+      "ga('create', '" . $ga_tracking_id . "', '" . $domain_name . "');" .
       "ga('send', 'pageview');" .
+    "</script>" .
+    "\n";
+    // Old ga.js code
+    echo "<script>" .
+      "var _gaq = _gaq || [];" .
+      "_gaq.push(['_setAccount', '" . $ga_tracking_id . "']);" .
+      "_gaq.push(['_trackPageview']);" .
+      "(function() {" .
+        "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;" .
+        "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';" .
+        "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);" .
+      "})();" .
     "</script>" .
     "\n";
   }
